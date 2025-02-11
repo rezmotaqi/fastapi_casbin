fd8932f464f493b9b3921ffece75bb4100ec912e
Rez <rezmotaqi@gmail.com>
2025-02-11 14:59:40 +0330

    stuff

--- app/routes/user.py
+++ app/routes/user.py
@@ -5,14 +5,13 @@ from fastapi import APIRouter, Depends
 from app.schemas.user import CreateUser
 from app.services.auth import auth_service
 from app.services.casbin import casbin_service
-from app.services.user import UserService
+from app.services.user import UserService, get_user_service
 
 router = APIRouter()
 
-user_service = UserService()
-
 
 @router.post("/", dependencies=[Depends(casbin_service.authorize)])
-async def create_user(data: CreateUser, token_data: dict = Depends(auth_service.verify_token)):
-	await user_service.create_user(data=data)
-	return {"message": "User created successfully"}
+async def create_user(data: CreateUser, token_data: dict = Depends(auth_service.verify_token),
+                      user_service: UserService = Depends(get_user_service)):
+    await user_service.create_user(data=data)
+    return {"message": "User created successfully"}
--- app/services/user.py
+++ app/services/user.py
@@ -1,22 +1,23 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, Depends
+from motor.motor_asyncio import AsyncIOMotorDatabase
 
 from app.schemas.user import CreateUser
 from app.tools.mongo import mongo_service
 
 
 class UserService:
-	def __init__(self, db=mongo_service.db):
-		self.db = db
+    def __init__(self, db: AsyncIOMotorDatabase):
+        self.db = db
 
-	async def create_user(self, data: CreateUser):
-		existing_user = await self.db["users"].find_one({"email":
-		data.email})
-		if existing_user:
-			raise HTTPException(status_code=400,
-								detail="User with this email already exists.")
+    async def create_user(self, data: CreateUser):
+        existing_user = await self.db["users"].find_one({"email": data.email})
+        if existing_user:
+            raise HTTPException(status_code=400, detail="User with this email already exists.")
 
-		inserted_user = await self.db["users"].insert_one(data.model_dump())
-		print(inserted_user.inserted_id)
+        inserted_user = await self.db["users"].insert_one(data.model_dump())
+        print(inserted_user.inserted_id)
 
 
-user_service = UserService()
+# Dependency Injection Function
+def get_user_service(db: AsyncIOMotorDatabase = Depends(lambda: mongo_service.db)):
+    return UserService(db)
